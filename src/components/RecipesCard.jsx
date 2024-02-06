92063060617678b5c8d68c4dff55828c03ebf3e0
Barbara Lan <barbie.lancuba@gmail.com>
2024-02-06 16:00:24 +0100

    trying our best

--- src/components/RecipesCard.jsx
+++ src/components/RecipesCard.jsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
+
 function RecipesCard(props) {
 const recipe = props.recipe;
 
 function displayCalories (recipe) {
-    return recipe.calories ? 
-
+    return recipe.calories >= 350 ? 'High calorie content ' : 'Low Calorie content'
 }
 
 
--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -2,51 +2,27 @@ import RecipesJson from './src/recipes.json'
 
 function RecipesList() {
 
-const [recipes, setRecipes] = useState (RecipesData)
+    const [recipes, setRecipes] = useState(RecipesData)
 
-function deleteCard(recipeId){ 
-const newRecipesArray = recipes.filter((recipes) => {
-return recipeId !== recipes.id
-});
-}
-}
+    function deleteCard(recipeId) {
+        const newRecipesArray = recipes.filter((recipes) => {
+            return recipeId !== recipes.id
+        });
+    }
 
-setRecipes(newRecipesArray) ;
+setRecipes(newRecipesArray);
 
 return (
-<>
-<ul className = 'cards-container'> 
-{
-recipes.map((recipes) => {
-return 
+    <ul className='cards-container'>
 
-}
+        {
+            recipes.map((recipes) => {
+                return
 
-
-
-
-  </ul>
-
-</>
-
+            })
+        }
+    </ul>
 )
-
-
-
-
-
-
-/* function RecipesList(props) {
-    const bookRecipes = props.recipe;
-
-    function displayRecipes(recipe) {
-        /*   if (movie.hasOscars) {
-              return '🏆'
-          } else {
-              return '💩'
-          } 
-
-        return calories ? '🏆' : '💩' Ternary operator, is not necessary to create a function for a single line
-
     }
-} */
+
+export default RecipesList
