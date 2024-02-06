6a3e1cf1f22a844808cb956a3c5f065f36b282e1
Barbara Lan <barbie.lancuba@gmail.com>
2024-02-06 17:36:53 +0100

    task 1 and 2 completed

--- src/components/RecipesCard.jsx
+++ src/components/RecipesCard.jsx
@@ -1,5 +1,19 @@
 function RecipesCard(props) {
-const recipe = props.recipe;
-    return recipe.calories >= 350 ? 'High calorie content ' : 'Low Calorie content'
+    const recipe = props.recipe;
+    const caloriesCount = recipe.calories >= 350 ? 'High calorie content ' : 'Low Calorie content'
+
+    function displayCalories(calorie) {
+        return caloriesCount
+    }
+
+    return (
+        <li className="recipe-card" key={recipe.id}>
+            <p>{recipe.name}</p>
+            <p>{recipe.calories}</p>
+            <p>{recipe.image}</p>
+            <p>{recipe.servings}</p>
+            {caloriesCount}
+        </li>
+    )
 }
 export default RecipesCard
