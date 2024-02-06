f8a696d1485955041ff046dba7c06e6f123b6045
Barbara Lan <barbie.lancuba@gmail.com>
2024-02-06 15:15:29 +0100

    first commit

---
+++ src/components/RecipesList.jsx
@@ -0,0 +1,16 @@
+import RecipesJson from './src/recipes.json'
+
+/* function RecipesList(props) {
+    const bookRecipes = props.recipe;
+
+    function displayRecipes(recipe) {
+        /*   if (movie.hasOscars) {
+              return '🏆'
+          } else {
+              return '💩'
+          } 
+
+        return calories ? '🏆' : '💩' Ternary operator, is not necessary to create a function for a single line
+
+    }
+} */
