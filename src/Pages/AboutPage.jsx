57074a7618c13ed49755c421ed3911d73b1b1f31
Barbara Lan <barbie.lancuba@gmail.com>
2024-02-13 15:30:47 +0100

    add div in aboutPage

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -2,9 +2,11 @@ function AboutPage() {
     return (
     <>
         <h2>About Us</h2>
+        <div className="abaoutPage">
         <p> Description of the project </p>
         <p>Member n 1 : Gavin Alexander</p>
         <p>Member n 1 : Barbara Lancuba</p>
+        </div>
     </>
     )
 }
