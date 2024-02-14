21b244eb16ffb350c1dcaa2fae0ea8a013fba706
Barbara Lan <barbie.lancuba@gmail.com>
2024-02-14 17:39:17 +0100

    more css

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,13 +1,20 @@
+import '../components/AboutPage.css'
+
 function AboutPage() {
     return (
-    <>
-        <h2>About Us</h2>
-        <div className="abaoutPage">
-        <p> Description of the project </p>
-        <p>Member n 1 : Gavin Alexander</p>
-        <p>Member n 1 : Barbara Lancuba</p>
-        </div>
-    </>
+        <>
+            <h2>About Us</h2>
+            <div className="abaoutPage">
+                <p> We decided to create an ibook for recipes;
+                    Here feel free to follow the actual recipes provedid,
+                    create ypur own recipes or edit them.
+                    You can also delete the recipes as you wish. </p>
+                <div className='creators'>
+                    <p> Member n 1 : Gavin Alexander </p>
+                    <p> Member n 2 : Barbara Lancuba </p>
+                </div>
+            </div>
+        </>
     )
 }
 export default AboutPage
